cc99726bc988bbda556542575528134ef78ba681
Eugene Gershnik <gershnik@users.noreply.github.com>
2024-11-19 10:16:26 -0800

    Reorganizing code

--- lib/inc/sys_string/impl/misc.h
+++ lib/inc/sys_string/impl/misc.h
@@ -9,8 +9,6 @@
 #error This header must not be included directly. Please include sys_string.h
 #endif
 
-#include <cstdarg>
-
 namespace sysstr
 {
     namespace util 
@@ -82,113 +80,6 @@ namespace sysstr
         }
     }
 
-    template<class Storage>
-    template<std::invocable<std::string_view> Func>
-    inline
-    auto sys_string_t<Storage>::print_with(Func func) const -> decltype(func(std::string_view{}))
-    {
-        if constexpr (std::ranges::contiguous_range<typename sys_string_t<Storage>::char_access> &&
-                        std::is_same_v<typename sys_string_t<Storage>::storage_type, char>)
-        {
-            typename sysstr::sys_string_t<Storage>::char_access access(*this);
-            return func(std::string_view(access.data(), access.size()));
-        } 
-        else 
-        {
-            return func(sysstr::sys_string_t<Storage>::utf8_access(*this));
-        }
-    }
-
-    #if SYS_STRING_WCHAR_T_IS_UTF16
-        template<class Storage>
-        template<std::invocable<std::wstring_view> Func>
-        inline
-        auto sys_string_t<Storage>::wprint_with(Func func) const -> decltype(func(std::wstring_view{}))
-        {
-            if constexpr (std::ranges::contiguous_range<typename sys_string_t<Storage>::char_access> &&
-                            std::is_same_v<typename sys_string_t<Storage>::storage_type, char16_t>)
-            {
-                typename sysstr::sys_string_t<Storage>::char_access access(*this);
-                return func(std::wstring_view((const wchar_t *)access.data(), access.size()));
-            } 
-            else 
-            {
-                return func(sysstr::sys_string_t<Storage>::utf16_access(*this));
-            }
-        }
-    #elif SYS_STRING_WCHAR_T_IS_UTF32
-        template<class Storage>
-        template<std::invocable<std::wstring_view> Func>
-        inline
-        auto sys_string_t<Storage>::wprint_with(Func func) const -> decltype(func(std::wstring_view{}))
-        {
-            if constexpr (std::ranges::contiguous_range<typename sys_string_t<Storage>::char_access> &&
-                            std::is_same_v<typename sys_string_t<Storage>::storage_type, char32_t>)
-            {
-                typename sysstr::sys_string_t<Storage>::char_access access(*this);
-                return func(std::wstring_view((const wchar_t *)access.data(), access.size()));
-            } 
-            else 
-            {
-                return func(sysstr::sys_string_t<Storage>::utf32_access(*this));
-            }
-        }
-    #endif
-
-}
-
-#if SYS_STRING_SUPPORTS_STD_FORMAT
-
-template<class Storage> struct std::formatter<sysstr::sys_string_t<Storage>> 
-{
-    template<class ParseContext>
-    constexpr auto parse(ParseContext & ctx) -> ParseContext::iterator
-    {
-        auto it = ctx.begin();
-        if (it != ctx.end() && *it != '}')
-            throw std::format_error("Invalid format args");
- 
-        return it;
-    }
-
-    template <typename FormatContext>
-    auto format(const sysstr::sys_string_t<Storage> & str, FormatContext & ctx) const -> decltype(ctx.out()) 
-    {
-        return str.print_with([&](const auto & view) -> decltype(ctx.out()) {
-            return std::ranges::copy(view, ctx.out()).out;
-        });
-    }
-};
-
-#if SYS_STRING_WCHAR_T_IS_UTF16 || SYS_STRING_WCHAR_T_IS_UTF32
-
-template<class Storage> struct std::formatter<sysstr::sys_string_t<Storage>, wchar_t> 
-{
-    template<class ParseContext>
-    constexpr auto parse(ParseContext & ctx) -> ParseContext::iterator
-    {
-        auto it = ctx.begin();
-        if (it != ctx.end() && *it != L'}')
-            throw std::format_error("Invalid format args");
- 
-        return it;
-    }
-
-    template <typename FormatContext>
-    auto format(const sysstr::sys_string_t<Storage> & str, FormatContext & ctx) const -> decltype(ctx.out()) 
-    {
-        return str.wprint_with([&](const auto & view) -> decltype(ctx.out()) {
-            return std::ranges::copy(view, ctx.out()).out;
-        });
-    }
-};
-
-#endif
-
-#endif
-
-namespace sysstr 
-{
     template<class Storage>
     inline
     auto sys_string_t<Storage>::to_lower() const -> sys_string_t<Storage>

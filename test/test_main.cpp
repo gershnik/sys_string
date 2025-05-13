9a40b7a0b5a7057b4a40564c37ebee4627569ff0
Eugene Gershnik <gershnik@users.noreply.github.com>
2025-05-12 19:55:22 -0700

    Detecting Python better

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -73,6 +73,15 @@ JNIEnv * g_env;
 
 #endif
 
+#if defined(SYS_STRING_USE_PYTHON) && defined(PYPY_VERSION)
+
+extern "C" {
+    void rpython_startup_code(void);
+    int pypy_setup_home(char *home, int verbose);
+}
+
+#endif
+
 int main(int argc, char** argv)
 {
     #if defined(__ANDROID__)
@@ -85,9 +94,16 @@ int main(int argc, char** argv)
         SetConsoleOutputCP(CP_UTF8);
     #endif
 
-    #if defined(SYS_STRING_USE_PYTHON) && !defined(PYPY_VERSION)
-        Py_Initialize();
+    #if defined(SYS_STRING_USE_PYTHON)
+        #if !defined(PYPY_VERSION)
+            Py_Initialize();
+        #else
+            rpython_startup_code();
+            pypy_setup_home(nullptr, 1);
+        #endif
     #endif
+
+    
     
     return doctest::Context(argc, argv).run();
 }
